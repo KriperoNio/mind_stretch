39968672513de0493ab3c07dede0e0de84015424
KriperoNio <kriperonio@yandex.com>
2025-07-02 15:51:32 +0300

    I noticed a flaw

--- lib/data/repository/local/storage_repository_impl.dart
+++ lib/data/repository/local/storage_repository_impl.dart
@@ -7,30 +7,34 @@ class StorageRepositoryImpl implements StorageRepository {
 
   const StorageRepositoryImpl({required this.prefs});
 
+  String get riddleKey => 'riddle';
+  String get titleArticleKey => 'title_article';
+  String get wordKey => 'word';
+
   @override
   Riddle? loadRiddle() {
-    final riddle = prefs.getString('riddle');
+    final riddle = prefs.getString(riddleKey);
     return riddle != null ? Riddle.fromString(riddle) : null;
   }
 
   @override
-  String? loadTitleArticle() => prefs.getString('title_article');
+  String? loadTitleArticle() => prefs.getString(titleArticleKey);
 
   @override
-  String? loadWord() => prefs.getString('word');
+  String? loadWord() => prefs.getString(wordKey);
 
   @override
   void saveRiddle({required String riddle}) {
-    prefs.setString('riddle', riddle);
+    prefs.setString(riddleKey, riddle);
   }
 
   @override
   void saveTitleArticle({required String titleArticle}) {
-    prefs.setString('title_article', titleArticle);
+    prefs.setString(titleArticleKey, titleArticle);
   }
 
   @override
   void saveWord({required String word}) {
-    prefs.setString('word', word);
+    prefs.setString(wordKey, word);
   }
 }
