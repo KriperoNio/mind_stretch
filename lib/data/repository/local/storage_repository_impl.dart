62ee5377643021424eb9bdcb7fc2eb3342315cd7
KriperoNio <kriperonio@yandex.com>
2025-07-02 16:05:22 +0300

    it wasn't a flaw ;)

--- lib/data/repository/local/storage_repository_impl.dart
+++ lib/data/repository/local/storage_repository_impl.dart
@@ -7,34 +7,34 @@ class StorageRepositoryImpl implements StorageRepository {
 
   const StorageRepositoryImpl({required this.prefs});
 
-  String get riddleKey => 'riddle';
-  String get titleArticleKey => 'title_article';
-  String get wordKey => 'word';
+  static const String _riddleKey = 'riddle';
+  static const String _titleArticleKey = 'title_article';
+  static const String _wordKey = 'word';
 
   @override
   Riddle? loadRiddle() {
-    final riddle = prefs.getString(riddleKey);
+    final riddle = prefs.getString(_riddleKey);
     return riddle != null ? Riddle.fromString(riddle) : null;
   }
 
   @override
-  String? loadTitleArticle() => prefs.getString(titleArticleKey);
+  String? loadTitleArticle() => prefs.getString(_titleArticleKey);
 
   @override
-  String? loadWord() => prefs.getString(wordKey);
+  String? loadWord() => prefs.getString(_wordKey);
 
   @override
   void saveRiddle({required String riddle}) {
-    prefs.setString(riddleKey, riddle);
+    prefs.setString(_riddleKey, riddle);
   }
 
   @override
   void saveTitleArticle({required String titleArticle}) {
-    prefs.setString(titleArticleKey, titleArticle);
+    prefs.setString(_titleArticleKey, titleArticle);
   }
 
   @override
   void saveWord({required String word}) {
-    prefs.setString(wordKey, word);
+    prefs.setString(_wordKey, word);
   }
 }
